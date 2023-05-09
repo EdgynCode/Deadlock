8ed7ecc1dd2161970837066636e3b743db52af29
EdgynCode <nguyenhuutan080603@gmail.com>
2023-05-10 00:23:16 +0700

    Add project files.

---
+++ deadlock.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include"support.h"
+using namespace std;
+
+int main() {
+	int numResources = 5;
+	int numProcesses = 3;
+	//vector<int> available(m);
+
+	// allocate
+	int** alloc = new int* [numResources];
+	for (int i = 0; i < numResources; i++) {
+		alloc[i] = new int[numProcesses];
+	}
+	int** max = new int* [numResources];
+	for (int i = 0; i < numResources; i++) {
+		max[i] = new int[numProcesses];
+	}
+	int** need = new int* [numResources];
+	for (int i = 0; i < numResources; i++) {
+		need[i] = new int[numProcesses];
+	}
+
+	readMatrix("alloc.txt", alloc, numResources, numProcesses);
+	readMatrix("max.txt", max, numResources, numProcesses);
+
+	calculateNeed(need, alloc, max, numResources, numProcesses);
+
+	cout << "Alloc: \n";
+	print(alloc, numResources, numProcesses);
+	cout << endl;
+	cout << "Max: \n";
+	print(max, numResources, numProcesses);
+	cout << endl;
+	cout << "Need: \n";
+	print(need, numResources, numProcesses);
+
+	// dispose
+	for (int i = 0; i < numResources; i++) {
+		delete[] alloc[i];
+		delete[] max[i];
+		delete[] need[i];
+	}
+	delete[] alloc;
+	delete[] max;
+	delete[] need;
+}
---
+++ support.h
@@ -0,0 +1,56 @@
+#pragma once
+#include<fstream>
+#include<string>
+#include<vector>
+using namespace std;
+
+void readMatrix(string fn, int** matrix, int m, int n) {
+	ifstream fi;
+	try {
+		fi.open(fn);
+		if (fi.is_open()) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					fi >> matrix[i][j];
+				}
+			}
+			fi.close();
+		}
+	}
+	catch (exception ex) {
+		cout << "Error while reading file...";
+	}
+}
+
+void calculateNeed(int** need, int** alloc, int** max, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			need[i][j] = max[i][j] - alloc[i][j];
+		}
+	}
+}
+
+void readVector(string fn, vector<int> vec, int n) {
+	ifstream fi;
+	int tmp;
+	try {
+		fi.open(fn);
+		if (fi.is_open()) {
+			while (fi >> tmp)
+				vec.push_back(tmp);
+		}
+		fi.close();
+	}
+	catch (exception ex) {
+		cout << "Error while reading file...";
+	}
+}
+
+void print(int** matrix, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
